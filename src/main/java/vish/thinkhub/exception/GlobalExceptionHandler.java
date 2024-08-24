812555ba0057a827b1351399e5bc34768da9ab08
VishaL6i9 <vishalkandakatla@gmail.com>
2024-08-24 21:53:45 +0530

    Fixed Error response not showing

--- src/main/java/vish/thinkhub/exception/GlobalExceptionHandler.java
+++ src/main/java/vish/thinkhub/exception/GlobalExceptionHandler.java
@@ -17,8 +17,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ExternalApiException.class)
     public ResponseEntity<ErrorResponse> handleExternalApiException(ExternalApiException ex) {
-        ErrorResponse error = new ErrorResponse("External API Error", ex.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.SERVICE_UNAVAILABLE);
+        if (ex.getMessage().contains("API rate exceeded")) {
+            ErrorResponse error = new ErrorResponse("API Rate Exceeded", "API rate limit exceeded. Please try again later.");
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).header("Location", "/error/api-rate-exceeded").build();
+        } else {
+            ErrorResponse error = new ErrorResponse("External API Error", ex.getMessage());
+            return new ResponseEntity<>(error, HttpStatus.SERVICE_UNAVAILABLE);
+        }
     }
 
     @ExceptionHandler(Exception.class)
@@ -26,5 +31,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorResponse error = new ErrorResponse("Internal Server Error", "An unexpected error occurred");
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
