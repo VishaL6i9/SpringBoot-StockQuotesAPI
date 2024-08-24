53458350c97f8c6e6e7ed21e4c779c4a697a6f88
VishaL6i9 <vishalkandakatla@gmail.com>
2024-08-24 21:54:23 +0530

    Fixed throwing error response

--- src/main/java/vish/thinkhub/exception/ExternalApiException.java
+++ src/main/java/vish/thinkhub/exception/ExternalApiException.java
@@ -1,12 +1,16 @@
 package vish.thinkhub.exception;
 
-
 public class ExternalApiException extends RuntimeException {
+
     public ExternalApiException(String message) {
         super(message);
     }
 
     public ExternalApiException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public ExternalApiException(Throwable cause) {
+        super(cause);
     }
 }
